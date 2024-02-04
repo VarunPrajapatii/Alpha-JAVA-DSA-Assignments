f43e2d6a48a7e9764544193a3e8365af1559140b
Varun Prajapati <varunprajapati123@gmail.com>
2024-02-04 18:20:58 +0530

    Assignment 1 completed

---
+++ _1_Variables_Questions.java
@@ -0,0 +1,105 @@
+// Question 1 : In a program, input 3 numbers : A, B and C. You have to output the average of
+// these 3 numbers.
+// Hint : Average of N numbers is sum of those numbers divided by N)
+
+// import java.util.*;
+
+// public class _1_Variables_Questions {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter 3 numbers to find their average");
+//         int A = sc.nextInt();
+//         int B = sc.nextInt();
+//         int C = sc.nextInt();
+//         System.out.println("Average is: " + (A+B+C)/3);        
+//     }
+// }
+
+
+
+
+
+
+// Question 2: In a program, input the side of a square. You have to output the area of the
+// square.
+// (Hint : area of a square is (side x side))
+
+// import java.util.*;
+
+// public class _1_Variables_Questions {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter side of square: ");
+//         int side = sc.nextInt();
+//         System.out.println("Area of Square is: " + (side*side));
+//     }
+// }
+
+
+
+
+
+
+
+// Question 3: Enter cost of 3 items from the user (using float data type) - a pencil, a pen and
+// an eraser. You have to output the total cost of the items back to the user as their bill.
+// (Add on : You can also try adding 18% gst tax to the items in the bill as an advanced problem)
+
+// import java.util.*;
+
+// public class _1_Variables_Questions {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter cost of pencil: ");
+//         float pencil = sc.nextFloat();
+//         System.out.println("Enter cost of pen: ");
+//         float pen = sc.nextFloat();
+//         System.out.println("Enter cost of eraser: ");
+//         float eraser = sc.nextFloat();
+//         float total = pencil + pen + eraser;
+//         System.out.println("Total: " + total);
+//         float gst = (total * 0.18f);
+//         System.out.println("Gst: " + gst);
+//         System.out.println("Grand Total: " + (total+gst));
+//     }
+// }
+
+
+
+
+
+
+
+// Question 4: What will be the type of result in the following Java code?
+
+
+// public class _1_Variables_Questions {
+//     public static void main(String args[]) {
+//         byte b= 4;
+//         char c = 'a';
+//         short s = 1000;
+//         int i = 1000;
+//         float f = 3.14f;
+//         double d = 99.9954;
+//         double result = (f*b) + (i%c) - (d*s);
+//         System.out.println(result);
+//     }
+// }
+
+
+
+
+
+
+
+// Question 5: (Advanced) Will the following statement give any error in Java?
+// int $ = 24;
+// Note - These questions are only to help you practice the concepts of this chapter. These are
+// not designed to test your mathematical skills, just to understand logic building using Java.
+
+// public class _1_Variables_Questions {
+//     public static void main(String args[]) {
+//         int $ = 24;
+//         System.out.println($);
+//     }
+// }
