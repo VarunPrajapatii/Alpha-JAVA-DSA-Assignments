d2584cf6ab77957f9d2e39a6fffd2742836830d1
Varun Prajapati <varunprajapati123@gmail.com>
2024-02-18 12:07:34 +0530

    Assignment 7 completed

--- Temp.java
+++ Temp.java
@@ -1,15 +1,27 @@
 import java.util.*;
 
 public class Temp {
-    public static void main(String args[]) {
-        char c = 'A';
-        // System.err.println(c++);
-        for(int i=1; i<=4; i++) {
-            for(int s = 1; s <= i; s++) {
-                System.out.print(c);
-                c++;
+    
+    public static void staircaseSearch(int[][] matrix, int key) {
+        int row=0, col=matrix[0].length-1;
+        while(row<matrix.length || col>=0 ) {
+            if(key==matrix[row][col]) {
+                System.out.println(row + " " + col);
+                return;
+            } else if (key<matrix[row][col]) {
+                col--;
+            } else {
+                row++;
             }
-            System.out.println();
         }
-    }
+    };
+    
+    public static void main(String args[]) {
+        int matrix[][] = {{10,20,30,40},
+                            {15,25,35,45},
+                            {27,29,37,48},
+                            {32,33,39,50}};
+
+        staircaseSearch(matrix, 33);
+    };
 }
