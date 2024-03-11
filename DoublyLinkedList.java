4fc02b7c2271ee4a0c9d8bac8f0156cf008e49e4
Varun Prajapati <varunprajapati123@gmail.com>
2024-03-11 13:28:37 +0530

    Doubly Linked List updated

---
+++ DoublyLinkedList.java
@@ -0,0 +1,134 @@
+public class DoublyLinkedList {
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    //print
+    public void print() {
+        Node temp = head;
+        if(head == null) {
+            System.out.println("Linked List is empty");
+            return;
+        }
+        while(temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+
+        }
+        System.out.print("null");
+        System.out.println();
+    }
+
+
+    //add
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if(tail == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
+
+    //remove
+    public int removeFirst() {
+        if(head == null) {
+            System.out.println("Linked List is empty");
+            return Integer.MIN_VALUE;
+        }
+        if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+        if(tail == null) {
+            System.out.println("Linked List is empty");
+            return Integer.MIN_VALUE;
+        }
+        if(size == 1) {
+            int val = tail.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = tail.data;
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+        return val;
+    }
+
+    public void reverse() {
+        Node curr = head;
+        Node prev = null;
+        Node next;
+
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.print();
+        System.out.println(dll.size);
+
+        dll.removeFirst();
+        dll.print();
+        System.out.println(dll.size);
+        dll.addFirst(1);
+        dll.print();
+        dll.reverse();
+        dll.print();
+
+    }
+
+}
+
+
